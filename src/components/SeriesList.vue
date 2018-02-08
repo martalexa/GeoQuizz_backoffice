550efb95b941698fec4fd9d1811db63b90a16af0
martalexa <alexandramartinpro@gmail.com>
2018-02-08 18:20:58 +0100

    design liste des series

--- src/components/SeriesList.vue
+++ src/components/SeriesList.vue
@@ -1,27 +1,36 @@
 <template>
 	<div>
-		<h1>Series List! <router-link to="/series/add">Add new</router-link></h1>
-		<table>
-			<tbody>
-				<th>Name</th>
-				<th>Distance</th>
-				<th>City</th>
-				<th>Actions</th>
-				<tr v-for="serie in series">
-					<td>{{serie.name}}</td>
-					<td>{{serie.distance}}</td>
-					<td>{{serie.city.name}}</td>
-					<td>
-						<button @click="deleteSerie(serie.id)">Supprimer</button>
-						<router-link :to="{name: 'edit_serie', params: {id: serie.id}}"><button>Modifier</button></router-link>
-						<router-link :to="{name: 'serie_photos', params: {id: serie.id}}"><button>Ajouter des photos</button></router-link>
-						<router-link :to="{name: 'serie_rules', params: {id: serie.id}}"><button>Ajouter des règles</button></router-link>
-					</td>
+		<h1 class="text-xs-center">Liste des séries <router-link to="/series/add"><v-icon color="light-green darken-1">add_circle</v-icon></router-link></h1>
+		</br>
 
-				</tr>
-			</tbody>
+		<v-layout row wrap>
+	          <v-flex xs12 xl6 offset-xl3>
+	            <table class="table is-striped">
+	              <thead>
+	                <tr>
+	                  <th>Nom</th>
+	                  <th>Distance (m)</th>
+	                  <th>Nom de la ville</th>
+										<th>Modifications</th>
+	                </tr>
+	              </thead>
+	              <tbody v-for="serie in series">
+	                <tr>
+	                  <td class="text-xs-center"><b>{{serie.name}}</b></td>
+	                  <td class="text-xs-center">{{serie.distance}}</td>
+	                  <td class="text-xs-center">{{serie.city.name}}</td>
+										<td class="text-xs-center">
+											<button @click="deleteSerie(serie.id)"><v-icon color="red darken-1">delete_forever</v-icon></button>
+											<router-link :to="{name: 'serie_photos', params: {id: serie.id}}" ><button><v-icon color="light-green darken-1">add_a_photo</v-icon></button></router-link>
+											<router-link :to="{name: 'edit_serie', params: {id: serie.id}}"><button><v-icon color="orange lighten-3">mode_edit</v-icon></button></router-link>
+											<router-link :to="{name: 'serie_rules', params: {id: serie.id}}"><button><v-icon color="cyan lighten-1">playlist_add</v-icon></button></router-link>
+										</td>
+	                </tr>
+	              </tbody>
+	            </table>
+	          </v-flex>
+	        </v-layout>
 
-		</table>
 	</div>
 </template>
 
