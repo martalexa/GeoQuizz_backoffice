5cba3683c3a2cc338803bef9da9c35f22e6c84e7
nico <bobelet.nicolas@gmail.com>
2018-02-10 09:26:35 +0100

    Edit Patch

--- src/components/EditSerie.vue
+++ src/components/EditSerie.vue
@@ -2,21 +2,21 @@
 	<div>
 
 		<v-flex xs12 sm5 offset-sm1>
-			<h1>Modifier la serie <span class="villeCurrent">{{villeCurrent.city.name}}</span></h1>
+			<h1>Modifier la serie <span class="current_serie">{{current_serie.name}} - {{current_serie.city.name}}</span></h1>
 		</v-flex>
 
 		<v-layout row wrap>
 			<v-flex xs12 sm4 offset-sm1>
 				<v-card>
-					<v-card-media v-if="this.imageVide" :src="'http://web.geoquizz.local:10085/uploads/'+villeCurrent.image" height="400px"></v-card-media>
+					<v-card-media v-if="this.imageVide" :src="'http://web.geoquizz.local:10085/uploads/'+current_serie.image" height="400px"></v-card-media>
 					<v-card-media v-else :src="prevImage" height="400px"></v-card-media>
 					<v-card-title primary-title>
 						<div>
 							<h3 class="headline mb-0">
-								<span v-if="serie.name.length!==0">{{serie.name}}</span> <span v-else class="villeCurrent">{{villeCurrent.name}}</span>
+								<span v-if="serie.name.length!==0">{{serie.name}}</span> <span v-else class="current_serie"></span>
 							</h3>
 							<div>
-								<span v-if="serie.distance!==null">{{serie.distance}}</span> <span v-else class="villeCurrent">{{villeCurrent.distance}}</span> <span class="unite">(metres)</span>
+								<span v-if="serie.distance!==null">{{serie.distance}}</span> <span v-else class="current_serie"></span> <span class="unite">(metres)</span>
 
 							</div>
 						</div>
@@ -31,9 +31,9 @@
 					<v-text-field color="green" :label="this.label" @click='pickFile' prepend-icon='attach_file'></v-text-field>
 					<input type="file"color="green" style="display: none" ref="image" accept="image/x-png,image/gif,image/jpeg" @change="fileChange">
 
-					<v-text-field color="green" type="text" v-model="serie.name" :label="villeCurrent.name"></v-text-field>
-					<v-select color="green" :label="villeCurrent.city.name" v-model="serie.city_id" item-value="id" item-text="name" :items="cities"></v-select>
-					<v-text-field color="green" type="text" v-model="serie.distance" :label="villeCurrent.distance"></v-text-field>
+					<v-text-field color="green" type="text"  v-model="serie.name" :label="current_serie.name"></v-text-field>
+					<v-select color="green" :label="current_serie.city.name" v-model="serie.city_id" item-value="id" item-text="name" :items="cities"></v-select>
+					<v-text-field color="green" type="text" v-model="serie.distance" :label="current_serie.distance"></v-text-field>
 
 					<v-btn @click="saveSerie">submit</v-btn>
 					<router-link to="/series/all"><v-btn color="red">Cancel</v-btn></router-link>
@@ -64,6 +64,9 @@ export default {
 		}
 	},
 	created() {
+		this.serie.name=this.current_serie.name
+		this.serie.distance=this.current_serie.distance
+		this.serie.city_id=this.current_serie.city.id
 		this.$store.dispatch('series/getCities').then((res) => {
 
 		}).catch((e) => {
@@ -107,7 +110,7 @@ export default {
 	},
 	computed: {
 		...mapGetters({cities: 'series/getCities'}),
-		...mapGetters({villeCurrent: 'series/getCurrentSerie'})
+		...mapGetters({current_serie: 'series/getCurrentSerie'})
 	}
 }
 </script>
@@ -122,7 +125,7 @@ h1{
 	margin-left:50px;
 	margin-bottom :20px
 }
-.villeCurrent{
+.current_serie{
 	color:grey;
 }
 .imageDefault{
